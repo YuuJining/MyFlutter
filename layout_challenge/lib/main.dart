e8b716384b8591e450ecd947428760c8571e4d57
Yujin Lee <yujin092444@gmail.com>
2020-09-02 00:50:12 +0900

    # Challenge2_1 - MiCard(add icon)

--- layout_challenge/lib/main.dart
+++ layout_challenge/lib/main.dart
@@ -34,7 +34,55 @@ class  Myapp extends StatelessWidget {
                       color: Colors.teal.shade100,
                       fontWeight: FontWeight.bold,
                     ),
-              )
+              ),
+              Container(
+                padding: EdgeInsets.all(10.0),
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                color: Colors.white,
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.phone,
+                      color: Colors.teal,
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      '+82 010 8223 4093',
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontFamily: 'ShrikHand',
+                        fontSize: 20.0,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.all(10.0),
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                color: Colors.white,
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.email,
+                      color: Colors.teal,
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      'yujin092444@gmail.com',
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontFamily: 'ShrikHand',
+                        fontSize: 20.0,
+                      ),
+                    )
+                  ],
+                ),
+              ),
             ],
           ),
         ),
