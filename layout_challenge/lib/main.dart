87f84d3a14bba3f675800191acbe29fe4d7ae997
Yujin Lee <yujin092444@gmail.com>
2020-09-08 23:15:29 +0900

    #Challenge2_1 - MiCard(ListTile, Card)

--- layout_challenge/lib/main.dart
+++ layout_challenge/lib/main.dart
@@ -12,6 +12,7 @@ class  Myapp extends StatelessWidget {
         backgroundColor: Colors.teal,
         body:SafeArea (
           child: Column (
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               CircleAvatar(
                 radius: 50.0,
@@ -35,54 +36,49 @@ class  Myapp extends StatelessWidget {
                       fontWeight: FontWeight.bold,
                     ),
               ),
-              Container(
-                padding: EdgeInsets.all(10.0),
-                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                color: Colors.white,
-                child: Row(
-                  children: <Widget>[
-                    Icon(
-                      Icons.phone,
-                      color: Colors.teal,
-                    ),
-                    SizedBox(
-                      width: 10.0,
-                    ),
-                    Text(
-                      '+82 010 8223 4093',
-                      style: TextStyle(
-                        color: Colors.teal.shade900,
-                        fontFamily: 'ShrikHand',
-                        fontSize: 20.0,
-                      ),
-                    )
-                  ],
+              SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child: Divider(
+                  color: Colors.teal.shade100,
                 ),
               ),
-              Container(
-                padding: EdgeInsets.all(10.0),
+              Card(
                 margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
                 color: Colors.white,
-                child: Row(
-                  children: <Widget>[
-                    Icon(
-                      Icons.email,
-                      color: Colors.teal,
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    '+82 010 8223 4093',
+                    style: TextStyle(
+                      color: Colors.teal.shade900,
+                      fontFamily: 'ShrikHand',
+                      fontSize: 17.0,
                     ),
-                    SizedBox(
-                      width: 10.0,
-                    ),
-                    Text(
-                      'yujin092444@gmail.com',
-                      style: TextStyle(
-                        color: Colors.teal.shade900,
-                        fontFamily: 'ShrikHand',
-                        fontSize: 20.0,
-                      ),
-                    )
-                  ],
+                  ),
                 ),
               ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                color: Colors.white,
+              child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.teal,
+                  ),
+                title: Text(
+                  'yujin092444@gmail.com',
+                  style: TextStyle(
+                    color: Colors.teal.shade900,
+                    fontFamily: 'ShrikHand',
+                    fontSize: 17.0,
+                      ),
+                    ),
+                  ),
+                ),
             ],
           ),
         ),
@@ -96,3 +92,43 @@ class  Myapp extends StatelessWidget {
 // Container는 오직 한 개의 child만 가진다
 // padding은 내용물과 container사이의 거리
 // 여러개의 자식을 갖고 싶으면 Column() Row() 사용
+
+//Row(
+//children: <Widget>[
+//Icon(
+//Icons.email,
+//color: Colors.teal,
+//),
+//SizedBox(
+//width: 10.0,
+//),
+//Text(
+//'yujin092444@gmail.com',
+//style: TextStyle(
+//color: Colors.teal.shade900,
+//fontFamily: 'ShrikHand',
+//fontSize: 20.0,
+//),
+//)
+//],
+//),
+
+//Row(
+//children: <Widget>[
+//Icon(
+//Icons.phone,
+//color: Colors.teal,
+//),
+//SizedBox(
+//width: 10.0,
+//),
+//Text(
+//'+82 010 8223 4093',
+//style: TextStyle(
+//color: Colors.teal.shade900,
+//fontFamily: 'ShrikHand',
+//fontSize: 20.0,
+//),
+//)
+//],
+//),
