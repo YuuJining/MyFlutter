323259508132e6182df83acd5b7a778c03dcccfa
Yujin Lee <yujin092444@gmail.com>
2020-09-18 22:37:33 +0900

    #Dicee - layout setting, adding flatbutton

--- dice/dice/lib/main.dart
+++ dice/dice/lib/main.dart
@@ -18,6 +18,32 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+      children: <Widget>[
+        Expanded(
+          child: FlatButton(
+              onPressed: (){
+                print('Left button got pressed.');
+              },//void callback이어서 ()-> docs참고
+              child: Image.asset('images/dice1.png'),
+          ),
+        ),
+        Expanded(
+          child: FlatButton(
+              onPressed: (){
+                print('Right button got pressed.');
+              },
+              child: Image.asset('images/dice2.png')
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
+
+
+//expanded 클래스 -> 사용가능한 공간에 의존하여 크기를 저절로 확장
+//flex 비율대로 크기 조절 됨
+// 노란 전구 누르면 위젯 remove나 추가 가능
