8786d66b495765176b64598ba00a80a6d53f1add
Yujin Lee <yujin092444@gmail.com>
2020-09-18 23:37:21 +0900

    # Dicee - adding function, change dice image

--- dice/dice/lib/main.dart
+++ dice/dice/lib/main.dart
@@ -16,6 +16,7 @@ void main() {
   );
 }
 
+
 class DicePage extends StatefulWidget {
   @override
   _DicePageState createState() => _DicePageState();
@@ -24,6 +25,14 @@ class DicePage extends StatefulWidget {
 class _DicePageState extends State<DicePage> {
   int leftDiceNumber = 1;
   int rightDiceNumber = 1;
+
+  void changeDiceFace() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
 
@@ -33,9 +42,7 @@ class _DicePageState extends State<DicePage> {
           Expanded(
             child: FlatButton(
               onPressed: (){
-                setState(() {
-                  leftDiceNumber = Random().nextInt(6) + 1;
-                });
+               changeDiceFace();
               },//void callback이어서 ()-> docs참고
               child: Image.asset('images/dice$leftDiceNumber.png'),
             ),
@@ -43,9 +50,7 @@ class _DicePageState extends State<DicePage> {
           Expanded(
             child: FlatButton(
                 onPressed: (){
-                  setState(() {
-                    rightDiceNumber = Random().nextInt(6) + 1;
-                  });
+                 changeDiceFace();
                 },
                 child: Image.asset('images/dice$rightDiceNumber.png')
             ),
